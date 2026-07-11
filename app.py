36046f4c29e6d513d5e80c8f01d72d0097b5440f
l0o0l-ops <smorimori926@gmail.com>
2026-07-11 16:46:33 +0900

    import update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 from google import genai
-from google.genai import genai
+from google import genai
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
